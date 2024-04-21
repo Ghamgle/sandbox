4c40f5aadda7065697d40bcee0657622f3c25b81
Ghamgle <13164107+Ghamgle@users.noreply.github.com>
2024-04-21 20:39:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 print("Hola Esta es mi primera prueba")
 print("Añadimos primera frase modificada web 4C")
-
-print("añado segunda 4c frase")
+print("añado7c frase")
+print("añado segunda 5c frase")
 
 
