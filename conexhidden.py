1051cf07bcb894270ed1a8b24dd2a60349796954
Ghamgle <toniocrevi@gmail.com>
2024-04-21 19:49:26 +0200

    Update conexhidden.py
    aaaa

--- conexhidden.py
+++ conexhidden.py
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
 print("fichero2")
 contrasena="mipassword"
+
+
+
+#no debería existir usuario
 userBD="usuario"
 
-=======
-print("fichero2")
->>>>>>> parent of 722b541 (Update conexhidden.py)
